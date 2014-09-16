84389393ae3fbfa63008c01ee4ede790a48c203c
Tessa Harmon <tessa@tessaharmon.com>
2014-09-15 21:58:22 -0400

    Add files

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    stylus: {
+      compile: {
+        files: {
+          'src/build/css/style.css': 'src/styl/main.styl'
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version']
+      },
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'src/build/css/style.css',
+        dest: 'src/build/css/'
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'css/style.min.css': 'src/build/css/style.css'
+        }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/'
+        }]
+      }
+    },
+    svgmin: {
+      options: {
+        plugins: [{
+          removeViewBox: false
+        }]
+      },
+      dist: {
+        files: {
+          'images/mural.min.svg': 'images/mural.svg'
+        }
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          'index.html': ['src/*.jade']
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['src/styl/*.styl'],
+        tasks: ['stylus', 'autoprefixer', 'cssmin'],
+        options: {
+          spawn: false
+        }
+      },
+      images: {
+        files: ['images/**/*.{png,jpg,gif}', 'images/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false
+        }
+      },
+      svg: {
+        files: 'images/*.svg',
+        tasks: ['imagemin'],
+        options: {
+          spawn: false
+        }
+      },
+      html: {
+        files: '*.html',
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: '1337',
+          base: './'
+        }
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['stylus', 'autoprefixer', 'cssmin', 'imagemin', 'svgmin']);
+
+  grunt.registerTask('dev', ['connect', 'watch']);
+}
